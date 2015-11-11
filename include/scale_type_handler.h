8ebffe719363badd5e234f522284fa2d142a26f3
Jayson Turner <jaysonturner@gmail.com>
2015-11-11 20:31:57 +0000

    scale type handler

---
+++ include/scale_type_handler.h
@@ -0,0 +1,8 @@
+#ifndef SCALE_TYPE_HANDLER_H
+#define SCALE_TYPE_HANDLER_H
+
+void sth_init();
+void sth_handle_index(int index);
+int sth_get_selected_scale_type();
+
+#endif
---
+++ src/scale_type_handler.c
@@ -0,0 +1,16 @@
+#include "scale_type_handler.h"
+
+void sth_init()
+{
+
+}
+
+void sth_handle_index(int index)
+{
+
+}
+
+int sth_get_selected_scale_type()
+{
+  return 0;
+}
